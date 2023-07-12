88c82dace9e72c0fee648679fee364baaee6f0ac
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-12 19:18:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ fetch('/bulletin/')
     // Extract the bulletin items from the fetched data
     const bulletinItems = extractBulletinItems(data);
 
-    // Update the first bulletin item with the latest data
-    updateBulletinItem(0, bulletinItems[0], '1');
-
-    // Shift the existing bulletin items
-    shiftBulletinItems(bulletinItems);
+    // Update the bulletin items with the extracted data
+    bulletinItems.forEach((item, index) => {
+      const itemNumber = index + 1;
+      updateBulletinItem(index, item, itemNumber);
+    });
   })
   .catch(error => {
     console.error('Error fetching bulletin data:', error);
@@ -24,13 +24,12 @@ function extractBulletinItems(data) {
   const itemElements = doc.querySelectorAll('.item.features-image');
 
   // Extract the bulletin data from each item
-  const bulletinItems = [];
-  itemElements.forEach(item => {
+  const bulletinItems = Array.from(itemElements).map((item, index) => {
     const date = item.querySelector('.item-content .item-title').textContent;
     const title = item.querySelector('.item-content .item-subtitle').textContent;
     const link = item.querySelector('.item-footer a').getAttribute('href');
 
-    bulletinItems.push({ date, title, link });
+    return { date, title, link };
   });
 
   return bulletinItems;
@@ -45,55 +44,4 @@ function updateBulletinItem(index, { date, title, link }, itemNumber) {
   dateElement.textContent = date;
   titleElement.textContent = title;
   linkElement.setAttribute('href', link);
-}
-
-// Function to shift the bulletin items
-function shiftBulletinItems(bulletinItems) {
-  // Check if there are at least two bulletin items
-  if (bulletinItems.length >= 2) {
-    // Get the second and third bulletin items
-    const secondItem = bulletinItems[1];
-    const thirdItem = bulletinItems[2];
-
-    // Update the second item with the data of the third item
-    if (secondItem) {
-      updateBulletinItem(1, thirdItem, '2');
-    }
-
-    // Remove the third item from the page
-    const thirdItemElement = document.querySelector('.item.features-image:nth-child(3)');
-    if (thirdItemElement) {
-      thirdItemElement.remove();
-    }
-
-    // Push the old second item to the third position
-    const newItemElement = createBulletinItemElement(secondItem, '3');
-    const rowElement = document.querySelector('.row');
-    if (rowElement && newItemElement) {
-      rowElement.appendChild(newItemElement);
-    }
-  }
-}
-
-// Function to create a bulletin item element
-function createBulletinItemElement({ date, title }, itemNumber) {
-  const itemElement = document.createElement('div');
-  itemElement.className = 'item features-image col-12 col-md-6 col-lg-4';
-
-  const itemContent = `
-    <div class="item-wrapper">
-      <div class="item-img">
-        <img src="assets/images/mbr-696x522.webp" alt="Mobirise Website Builder" title="">
-      </div>
-      <div class="item-content">
-        <h5 id="bulletin-date-${itemNumber}" class="item-title mbr-fonts-style display-4"><em>${date}</em></h5>
-        <h6 id="bulletin-title-${itemNumber}" class="item-subtitle mbr-fonts-style mt-1 display-7"><strong>${title}</strong></h6>
-      </div>
-      <div class="mbr-section-btn item-footer mt-2"><a href="" class="btn item-btn btn-primary-outline display-7" target="_blank">Read More &gt;</a></div>
-    </div>
-  `;
-
-  itemElement.innerHTML = itemContent;
-
-  return itemElement;
 }
