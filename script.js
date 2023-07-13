705a414e6c8be690f9de76a74b7d4efe1bcfef5d
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 01:03:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,36 +13,26 @@ async function fetchLatestBulletins() {
 function updateBulletinElements(latestBulletins) {
   const bulletinElements = document.querySelectorAll('.item.features-image');
 
-  const formatDate = (dateString) => {
-    const parts = dateString.split('-');
-    const day1 = parts[0];
-    const day2 = parts[1];
-    const month = parts[2];
-    const year = parts[3];
+  const getFormattedTitle = (title) => {
+    const parts = title.split('-');
+    const sunday = parts[0];
+    const year = parts[4];
 
-    const monthIndex = parseInt(month) - 1;
-    const monthName = monthNames[monthIndex];
-    return `${day1}-${day2}, ${monthName} ${year}`;
+    return `${sunday} Sunday Ordinary Year ${year}`;
   };
 
-  const monthNames = [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ];
-
   latestBulletins.forEach((bulletin, index) => {
-    const itemDate = bulletinElements[index].querySelector('.item-title');
-    const itemTitle = bulletinElements[index].querySelector('.item-subtitle');
+    const itemTitle = bulletinElements[index].querySelector('.item-title');
+    const itemSubtitle = bulletinElements[index].querySelector('.item-subtitle');
     const readMoreBtn = bulletinElements[index].querySelector('.item-footer a');
 
-    const formattedDate = formatDate(bulletin.title.replace('.pdf', ''));
-    const formattedTitle = bulletin.title.replace('.pdf', '');
-
-    itemDate.textContent = formattedDate;
+    const formattedTitle = getFormattedTitle(bulletin.title.replace('.pdf', ''));
     itemTitle.innerHTML = `<em>${formattedTitle}</em>`;
+    itemSubtitle.textContent = ''; // Remove the subtitle
     readMoreBtn.href = bulletin.href;
   });
 }
+
 
 (async function () {
   const latestBulletins = await fetchLatestBulletins();
