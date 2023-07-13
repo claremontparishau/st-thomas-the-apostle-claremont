c228c2bb7f6cfe6c48e74f0a4f445df58a0008bf
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 00:22:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ async function fetchLatestBulletins() {
   ];
 
   return bulletins.map(({ name, url }) => {
-    const [_, monthStr, dayStr, yearStr] = name.match(/^(\d{1,2})-(\w+)-(\d{4})/);
+    const match = name.match(/^(\d{1,2})-(\w+)-(\d{4})/);
+    if (!match) {
+      console.error(`Invalid file name format: ${name}`);
+      return null;
+    }
+    const [_, dayStr, monthStr, yearStr] = match;
     const monthIndex = monthNames.findIndex(name => name.startsWith(monthStr));
     const monthName = monthNames[monthIndex];
     const dayNum = parseInt(dayStr);
@@ -19,36 +24,5 @@ async function fetchLatestBulletins() {
     const title = `${dayNum}${daySuffix} ${monthName} Ordinary Year A`;
     const href = url;
     return { title, href };
-  });
+  }).filter(bulletin => bulletin != null);
 }
-
-function getDaySuffix(dayNum) {
-  if (dayNum >= 11 && dayNum <= 13) {
-    return 'th';
-  }
-  switch (dayNum % 10) {
-    case 1: return 'st';
-    case 2: return 'nd';
-    case 3: return 'rd';
-    default: return 'th';
-  }
-}
-
-function updateBulletinElements(latestBulletins) {
-  const bulletinElements = document.querySelectorAll('.item.features-image');
-
-  latestBulletins.forEach((bulletin, index) => {
-    const itemTitle = bulletinElements[index].querySelector('.item-title');
-    const itemSubtitle = bulletinElements[index].querySelector('.item-subtitle');
-    const readMoreBtn = bulletinElements[index].querySelector('.item-footer a');
-
-    itemTitle.innerHTML = `<em>${bulletin.title}</em>`;
-    itemSubtitle.textContent = ''; // Remove the subtitle
-    readMoreBtn.href = bulletin.href;
-  });
-}
-
-(async function () {
-  const latestBulletins = await fetchLatestBulletins();
-  updateBulletinElements(latestBulletins);
-})();
