7505039e2697021ca3bed86c56db8c9412dfb115
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 00:46:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+async function fetchLatestBulletins() {
+  const response = await fetch('/bulletin-manifest.json');
+  const manifest = await response.json();
+  const bulletins = manifest.slice(0, 3);
+
+  return bulletins.map(({ name, url }) => {
+    const title = name.replace('.pdf', '');
+    const href = url;
+    return { title, href };
+  });
+}
+
 function updateBulletinElements(latestBulletins) {
   const bulletinElements = document.querySelectorAll('.item.features-image');
 
   const formatDate = (dateString) => {
     const [sunday, ordinary, month, days, year] = dateString.split('-');
     const formattedMonth = new Date(`${month} 1, ${year}`).toLocaleString('en-US', { month: 'long' });
-    const formattedDays = parseInt(days) > 9 ? days : `0${days}`;
 
-    return `${formattedMonth} ${formattedDays}-${parseInt(formattedDays) + 1}, ${year}`;
-  };
-
-  const getFormattedTitle = (title) => {
-    const parts = title.split('-');
-    const sunday = parts[0];
-    const year = parts[4];
-
-    return `${sunday}th Sunday Ordinary Year ${year}`;
+    return `${formattedMonth} ${days}-${parseInt(days) + 1}, ${year}`;
   };
 
   latestBulletins.forEach((bulletin, index) => {
@@ -23,7 +26,10 @@ function updateBulletinElements(latestBulletins) {
     const readMoreBtn = bulletinElements[index].querySelector('.item-footer a');
 
     const formattedDate = formatDate(bulletin.title.replace('.pdf', ''));
-    const formattedTitle = getFormattedTitle(bulletin.title.replace('.pdf', ''));
+    const parts = bulletin.title.split('-');
+    const sunday = parts[0];
+    const year = parts[4];
+    const formattedTitle = `${sunday} Sunday Ordinary Year ${year}`;
 
     itemDate.textContent = formattedDate;
     itemTitle.innerHTML = `<em>${formattedTitle}</em>`;
@@ -31,7 +37,7 @@ function updateBulletinElements(latestBulletins) {
   });
 }
 
-
 (async function () {
   const latestBulletins = await fetchLatestBulletins();
   updateBulletinElements(latestBulletins);
+})();
