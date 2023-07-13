9ec67c28ad7fc2d0b45a09c1ef8bb0d45d47dd74
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 00:25:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ async function fetchLatestBulletins() {
   const manifest = await response.json();
   const bulletins = manifest.slice(0, 3);
 
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
   return bulletins.map(({ name, url }) => {
-    const title = name.replace('.pdf', '');
+    const match = name.match(/^(\d{1,2})\w*-(\w+)-(\d{4})/);
+    if (!match) {
+      console.error(`Invalid file name format: ${name}`);
+      return null;
+    }
+    const [_, dayStr, monthStr, yearStr] = match;
+    const monthIndex = monthNames.findIndex(name => name.startsWith(monthStr));
+    const monthName = monthNames[monthIndex];
+    const dayNum = parseInt(dayStr);
+    const daySuffix = getDaySuffix(dayNum);
+    const yearNum = parseInt(yearStr);
+    const yearShort = yearNum.toString().slice(-2);
+    const title = `${dayNum}${daySuffix} ${monthName} Ordinary Year A`;
     const href = url;
     return { title, href };
-  });
+  }).filter(bulletin => bulletin != null);
 }
 
 function updateBulletinElements(latestBulletins) {
