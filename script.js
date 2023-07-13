6816774a7457bf372a7461f0e007bdd39e4944fd
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 00:03:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function getFormattedDate(dateStr) {
+  const date = new Date(dateStr);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
+async function fetchLatestBulletins() {
+  const response = await fetch('/bulletin-manifest.json');
+  const manifest = await response.json();
+  const bulletins = manifest.slice(0, 3);
+
+  return bulletins.map(({ name, url }) => {
+    const dateMatch = name.match(/(\d{4}-\d{2}-\d{2})/);
+    const dateStr = dateMatch ? dateMatch[1] : '';
+    const titleStr = name.replace(/(\.pdf)$/i, '').replace(/-/g, ' ').replace(/\d{4}-\d{2}-\d{2}/, '').trim();
+    const formattedDate = getFormattedDate(dateStr);
+    const formattedTitle = getFormattedTitle(titleStr);
+    const title = `${formattedTitle} - ${formattedDate}`;
+    const href = url;
+    return { title, href };
+  });
+}
+
 async function fetchLatestBulletins() {
   const response = await fetch('/bulletin-manifest.json');
   const manifest = await response.json();
