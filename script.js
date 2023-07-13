38a95da4e147531649b82ad5e327c10122f0d744
jerekjoseph <60007795+jerekjoseph@users.noreply.github.com>
2023-07-14 00:27:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,18 @@ async function fetchLatestBulletins() {
   ];
 
   return bulletins.map(({ name, url }) => {
-    const match = name.match(/^(\d{1,2})\w*-(\w+)-(\d{4})/);
+    const match = name.match(/^(\d{1,2})(st|nd|rd|th)?-?(\w+)?-?(\d{4})/);
     if (!match) {
       console.error(`Invalid file name format: ${name}`);
       return null;
     }
-    const [_, dayStr, monthStr, yearStr] = match;
+    const [_, dayStr, daySuffix, monthStr, yearStr] = match;
     const monthIndex = monthNames.findIndex(name => name.startsWith(monthStr));
     const monthName = monthNames[monthIndex];
     const dayNum = parseInt(dayStr);
-    const daySuffix = getDaySuffix(dayNum);
     const yearNum = parseInt(yearStr);
     const yearShort = yearNum.toString().slice(-2);
-    const title = `${dayNum}${daySuffix} ${monthName} Ordinary Year A`;
+    const title = `${dayNum}${daySuffix ? daySuffix : ''} ${monthName} Ordinary Year A`;
     const href = url;
     return { title, href };
   }).filter(bulletin => bulletin != null);
